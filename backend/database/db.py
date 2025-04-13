d1f53b12ea03fea53eaf63f9976b9aa7a37ad33e
gabrielh063 <gabrielhalves006@gmail.com>
2025-04-13 20:47:48 -0300

    a

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 class database:
-    def __init__(self, db_name)
-    self.conn = sqlite3.connect(db_name)
-    self.cursor = self.conn.cursor()
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
 
     def execute(self, query, params):
         if params is None:
