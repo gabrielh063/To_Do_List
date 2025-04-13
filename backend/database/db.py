e0d83fc3cc78243c81e2f6d51c888309567ad936
otavio-alvxs <otavioalvescbp@gmail.com>
2025-04-13 20:26:03 -0300

    inicio_arquivo_banco

---
+++ backend/database/db.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+class database:
+    def __init__(self, db_name)
+    self.conn = sqlite3.connect(db_name)
+    self.cursor = self.conn.cursor()
+
+    def execute(self, query, params):
+        if params is None:
+            self.cursor.execute(query)
+        else:
+            self.cursor.execute(query, params)
+        self.conn.commit()
+
+    
