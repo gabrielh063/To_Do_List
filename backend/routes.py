5de5718b70213ced21ebf8183d52f9924c775af2
gabrielh063 <gabrielhalves006@gmail.com>
2025-04-13 22:18:06 -0300

    a

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,20 @@
+from flask import Blueprint
 from controllers.task_controller import get_tasks, add_task, update_task, delete_task
 
-task_blueprint = Blueprint('tasks', __name__)
+task_bp = Blueprint('tasks', __name__)
 
 @task_bp.route('/tasks', methods='GET')
-    def list_tasks():
-        return get_tasks()
+def list_tasks():
+    return get_tasks()
+
+@task_bp.route('/tasks', methods='POST')
+def new_task():
+    return add_task()
+
+@task_bp.route('/tasks/<id>/', methods="PUT")
+def task_update(id):
+    return update_task(id)
+
+@task_bp.route('/tasks/<id>/', methods='DELETE')
+def remove_task(id):
+    return delete_task(id)
