f8f60918ea78cbce6a031b36fbab50b10f694b4d
gabrielh063 <gabrielhalves006@gmail.com>
2025-04-14 19:25:36 -0300

    Finalização

--- backend/controllers/task_controller.py
+++ backend/controllers/task_controller.py
@@ -1,54 +1,97 @@
 from flask import jsonify, request
 from database.db import Database as db
 from models.task_model import Task
+from logger import Logger
+
+# Obtém a instância do Logger
+logger = Logger.get_instance()
 
 def get_tasks():
     try:
-        query = "SELECT * FROM tasks"
+        query = "SELECT * FROM TASKS"
         results = db.execute(query)
         
         # Cria uma lista de objetos Task
-        tasks = [Task(**task) for task in results]
+        TASKS = [Task(**task) for task in results]
         
         # Converte cada objeto Task para um dicionário
-        tasks_dict = [task.to_dict() for task in tasks]
+        tasks_dict = [task.to_dict() for task in TASKS]
         
+        logger.info("Tarefas obtidas com sucesso.")
         return jsonify(tasks_dict), 200
     except Exception as e:
-        print(e)
+        logger.error(f"Falha ao obter tarefas: {e}")
         return jsonify({"error": "Falha ao obter tarefas"}), 500
+
+def get_task_by_id(task_id):
+    try:
+        query = "SELECT * FROM TASKS WHERE ID_TASK = %s"
+        params = (task_id,)
+        result = db.execute(query, params)
+
+        if result and len(result) > 0:
+            task = Task(**result[0])
+            return jsonify(task.to_dict()), 200
+        else:
+            return jsonify({"error": "Tarefa não encontrada"}), 404
+    except Exception as e:
+        print(f"Erro ao buscar tarefa: {e}")
+        return jsonify({"error": "Falha ao buscar tarefa"}), 500
 
 def add_task():
     try:
         data = request.get_json()
-        query = "INSERT INTO tasks (TASK_TITLE, TASK_DESC, TASK_IS_DONE) VALUES (%s, %s, %s)"
+        query = "INSERT INTO TASKS (TASK_TITLE, TASK_DESC, TASK_IS_DONE) VALUES (%s, %s, %s)"
         params = (data['TASK_TITLE'], data['TASK_DESC'], data['TASK_IS_DONE'])
         db.execute(query, params)
         
+        logger.info(f"Tarefa '{data['TASK_TITLE']}' adicionada com sucesso.")
         return jsonify({"message": "Tarefa adicionada com sucesso"}), 201
     except Exception as e:
-        print(e)
+        logger.error(f"Falha ao adicionar tarefa: {e}")
         return jsonify({"error": "Falha ao adicionar tarefa"}), 500
 
-def update_task(task_id):
+def update_task(TASK_ID):
     try:
         data = request.get_json()
-        query = "UPDATE tasks SET TASK_TITLE = %s, TASK_DESC = %s, TASK_IS_DONE = %s WHERE task_id = %s"
-        params = (data['TASK_TITLE'], data['TASK_DESC'], data['TASK_IS_DONE'], task_id)
+
+        # Cria uma lista de pares de chave-valor a serem atualizados
+        fields_to_update = []
+
+        # Verifique se os campos estão presentes e adicione à lista de atualização
+        if 'TASK_TITLE' in data:
+            fields_to_update.append(f"TASK_TITLE = %s")
+        if 'TASK_DESC' in data:
+            fields_to_update.append(f"TASK_DESC = %s")
+        if 'TASK_IS_DONE' in data:
+            fields_to_update.append(f"TASK_IS_DONE = %s")
+
+        if not fields_to_update:
+            return jsonify({"error": "Nenhum dado para atualizar"}), 400
+        
+        # Monta a query dinamicamente
+        set_clause = ", ".join(fields_to_update)
+        query = f"UPDATE TASKS SET {set_clause} WHERE ID_TASK = %s"
+        
+        # Organiza os parâmetros para a query
+        params = tuple(data[key] for key in data.keys()) + (TASK_ID,)
+        
+        # Executa a query
         db.execute(query, params)
         
         return jsonify({"message": "Tarefa atualizada com sucesso"}), 200
     except Exception as e:
-        print(e)
+        print(f"Erro ao atualizar tarefa: {e}")
         return jsonify({"error": "Falha ao atualizar tarefa"}), 500
 
-def delete_task(task_id):
+def delete_task(ID_TASK):
     try:
-        query = "DELETE FROM tasks WHERE task_id = %s"
-        params = (task_id,)
+        query = "DELETE FROM TASKS WHERE ID_TASK = %s"
+        params = (ID_TASK,)
         db.execute(query, params)
         
+        logger.info(f"Tarefa {ID_TASK} removida com sucesso.")
         return jsonify({"message": "Tarefa removida com sucesso"}), 200
     except Exception as e:
-        print(e)
+        logger.error(f"Falha ao remover tarefa: {e}")
         return jsonify({"error": "Falha ao remover tarefa"}), 500
--- backend/routes.py
+++ backend/routes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint
-from controllers.task_controller import get_tasks, add_task, update_task, delete_task
+from controllers.task_controller import get_tasks, add_task, update_task, delete_task, get_task_by_id
 
 task_bp = Blueprint('tasks', __name__)
 
 @task_bp.route('/tasks', methods=['GET'])
 def list_tasks():
     return get_tasks()
+
+@task_bp.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    return get_task_by_id(task_id)
 
 @task_bp.route('/tasks', methods=['POST'])
 def new_task():
